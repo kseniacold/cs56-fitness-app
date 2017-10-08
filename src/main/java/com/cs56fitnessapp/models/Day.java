f40a1e34c4512ff99a7fadaaac21f93f40c9bbe1
Saori Shigehisa <kumakichi3173@gmail.com>
2017-10-08 12:40:06 -0700

    added to dayimpliment

--- src/main/java/com/cs56fitnessapp/models/Day.java
+++ src/main/java/com/cs56fitnessapp/models/Day.java
@@ -1,13 +1,33 @@
 package com.cs56fitnessapp.models;
 
 /**
- * @author Ksenia Koldaeva
- * Created: 9/17/17
- * Last Updated: 10/1/17
+ * @author Saori Shigehisa
+ * Created: 10/6/17
+ * Last Updated: 10/7/17
  */
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Day implements ActivityFacts, DietFacts {
 
+    /** Date */
+    private Date date;
+
+    /** User */
+    private User user;
+
+    /** calorieGoal */
+    private double calorieGoal;
+
+    /** WorkoutList */
+    private ArrayList < Workout > workoutList;
+
+    /** MealList */
+    private ArrayList < Meal > mealList;
+
+    public boolean isToday() { return false; }
+
+    public double getCaloriesLeft() { return 0.0; }
 
     /* ActivityFacts Interface */
     @Override
