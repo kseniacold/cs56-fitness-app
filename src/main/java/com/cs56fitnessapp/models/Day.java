3e1127ca095b8be0ba79963dbda57e2304bbb444
Saori Shigehisa <kumakichi3173@gmail.com>
2017-10-14 23:56:54 -0700

    added and changed all things you commented on github

--- src/main/java/com/cs56fitnessapp/models/Day.java
+++ src/main/java/com/cs56fitnessapp/models/Day.java
@@ -3,15 +3,17 @@ package com.cs56fitnessapp.models;
 /**
  * @author Saori Shigehisa
  * Created: 10/6/17
- * Last Updated: 10/7/17
+ * Last Updated: 10/14/17
  */
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public class Day implements ActivityFacts, DietFacts {
 
-    /** Date */
-    private Date date;
+    /** date */
+    private LocalDate date;
 
     /** User */
     private User user;
@@ -25,8 +27,28 @@ public class Day implements ActivityFacts, DietFacts {
     /** MealList */
     private ArrayList < Meal > mealList;
 
-    public boolean isToday() { return false; }
+    public Day (LocalDate date, User user, double caloriesGoal, ArrayList < Workout > workoutList, ArrayList < Meal > mealList){
+      this.date = date;
+      this.user = user;
+      this.calorieGoal = calorieGoal;
+      this.workoutList = workoutList;
+      this.mealList = mealList;
+    }
 
+
+
+    public boolean isToday() {
+        LocalDate now = LocalDate.now();
+
+        if (now.getYear() == date.getYear() && now.getMonth() == date.getMonth() && now.getDayOfMonth() == date.getDayOfMonth()) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    //Kcal ~= METS * bodyMassKg * timePerformingHours
     public double getCaloriesLeft() { return 0.0; }
 
     /* ActivityFacts Interface */
