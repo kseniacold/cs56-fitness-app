349df3bf5626b7204f4c2df3be6cb01ab3b5880c
Saori Shigehisa <kumakichi3173@gmail.com>
2017-10-24 10:16:40 -0700

    created implements in Day class

--- src/main/java/com/cs56fitnessapp/models/Day.java
+++ src/main/java/com/cs56fitnessapp/models/Day.java
@@ -5,6 +5,8 @@ package com.cs56fitnessapp.models;
  * Created: 10/6/17
  * Last Updated: 10/14/17
  */
+import com.cs56fitnessapp.models.workout.Workout;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -27,60 +29,82 @@ public class Day implements ActivityFacts, DietFacts {
     /** MealList */
     private ArrayList < Meal > mealList;
 
-    public Day (LocalDate date, User user, double caloriesGoal, ArrayList < Workout > workoutList, ArrayList < Meal > mealList){
+    public Day (LocalDate date, User user){
       this.date = date;
       this.user = user;
-      this.calorieGoal = calorieGoal;
-      this.workoutList = workoutList;
-      this.mealList = mealList;
     }
-
 
 
     public boolean isToday() {
         LocalDate now = LocalDate.now();
 
-        if (now.getYear() == date.getYear() && now.getMonth() == date.getMonth() && now.getDayOfMonth() == date.getDayOfMonth()) {
-            return true;
-        } else {
-            return false;
-
-        }
+        return now.getYear() == date.getYear() && now.getMonth() == date.getMonth() && now.getDayOfMonth() == date.getDayOfMonth();
     }
 
-    //Kcal ~= METS * bodyMassKg * timePerformingHours
-    public double getCaloriesLeft() { return 0.0; }
+    // TODO:
+    // Kcal ~= METS * bodyMassKg * timePerformingHours
+    public double getCaloriesLeft() {
+        return 0;
+    }
 
     /* ActivityFacts Interface */
     @Override
     public int getCaloriesOut() {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < workoutList.size(); ++i) {
+            Workout workout = workoutList.get(i);
+            sum += workout.getCaloriesOut();
+        }
+        return sum;
     }
 
     @Override
-    public int getActiveTimeMins() {
+    public int getActiveTimeMins()
+    {
         return 0;
     }
 
     /* DietFacts Interface */
     @Override
     public int getCaloriesIn() {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < mealList.size(); ++i) {
+            Meal meal = mealList.get(i);
+            sum += meal.getCaloriesIn();
+        }
+        return sum;
     }
 
     @Override
     public TotalFat getTotalFatIn() {
-        return null;
+        double sum = 0.0;
+        for (int i = 0; i < mealList.size(); ++i) {
+            Meal meal = mealList.get(i);
+            TotalFat totalfat = meal.getTotalFatIn();
+            sum += totalfat.getTotalFat();
+        }
+        return new TotalFat(sum);
     }
 
     @Override
     public TotalCarb getTotalCarbIn() {
-        return null;
+        double sum = 0.0;
+        for (int i = 0; i < mealList.size(); ++i) {
+            Meal meal = mealList.get(i);
+            TotalCarb totalcarb = meal.getTotalCarbIn();
+            sum += totalcarb.getTotalCarb();
+        }
+        return new TotalCarb(sum);
     }
 
     @Override
     public double getProteinIn() {
-        return 0;
+        double sum = 0.0;
+        for (int i = 0; i < mealList.size(); ++i) {
+            Meal meal = mealList.get(i);
+            sum += meal.getProteinIn();
+        }
+        return sum;
     }
 
     public static void main(String [] args) {
